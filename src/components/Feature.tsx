9f5935abd3d4c2ba2a9d7172d4d4fc74abf77727
Andrés Olivé <andresolive@gmail.com>
2022-09-07 13:12:20 +0200

    refactor: feature component

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -9,9 +9,9 @@ const Feature = (props: FeatureProps) => {
   const { icon, children, ...rest } = props;
 
   return (
-    <HStack {...rest} spacing='24px'>
-      <Icon as={icon} boxSize='48px'></Icon>
-      <Text fontSize='18px' fontWeight='700'>
+    <HStack {...rest} spacing='6'>
+      <Icon as={icon} boxSize='12'></Icon>
+      <Text fontSize='lg' fontWeight='bold'>
         {children}
       </Text>
     </HStack>
