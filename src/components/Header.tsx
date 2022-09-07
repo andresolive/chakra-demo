7632ead918d335072bc93f627a913fa0865d17d6
Andrés Olivé <andresolive@gmail.com>
2022-09-07 13:10:32 +0200

    refactor: header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,19 +2,19 @@ import { Box, Heading, Text } from '@chakra-ui/react';
 
 const Header = () => {
   return (
-    <Box pb='112px' as='section'>
+    <Box pb='28' as='section'>
       <Box
-        color='#F7FAFC'
-        bg='#6B46C1'
+        color='gray.50'
+        bg='purple.600'
         pt='50px'
         pb='190px'
-        px='32px'
+        px='8'
         textAlign='center'
       >
-        <Heading fontWeight='800' fontSize='48px'>
+        <Heading fontWeight='extrabold' fontSize='5xl'>
           Simple pricing for your business
         </Heading>
-        <Text fontWeight='500' fontSize='24px' pt='16px'>
+        <Text fontWeight='medium' fontSize='2xl' pt='4'>
           Plans that are carefully crafted to suit your business.
         </Text>
       </Box>
